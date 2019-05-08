363c7847d53b6e406b96929e80cb690abe0037cc
christians86 <sennes86@online.no>
2019-05-08 07:26:41 +0200

    driverfix

--- src/test/java/Utils/globalparams.java
+++ src/test/java/Utils/globalparams.java
@@ -5,7 +5,7 @@ public class globalparams {
     public static final String baseURL   = "https://www.teammodels.no/";
 
     public static final String driverIEPath   = "IEDriverServer314.exe";
-    public static final String driverChromePath   = "C:\\Users\\Admin\\IdeaProjects\\teamno\\src\\main\\resources\\chromedriver32.exe";
+    public static final String driverChromePath   = "C:\\Users\\senne\\IdeaProjects\\teamautomatisering\\src\\main\\resources\\chromedriver_v74.exe";
 
 
 }
